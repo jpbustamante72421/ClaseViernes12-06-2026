ab0df01704c9a0a7b419f9cbe1a099758e9e162c
Juan Pablo Bustmanate <72421leo@universidaddeleon.edu.mx>
2026-06-22 07:14:32 -0600

    corrección

--- loads/load_menu.py
+++ loads/load_menu.py
@@ -1,19 +1,16 @@
 
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
-# Importamos la clase del diálogo que creaste antes
 from loads.load_lista_enlazada_simple import DialogoListaEnlazada
 
 class MenuListaEnlazada(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("ui/ListaEnlazada.ui", self) # Asegúrate de que esta ruta sea correcta
+        uic.loadUi("ui/ListaEnlazada.ui", self) 
 
-        # CONEXIÓN: Al hacer clic en la acción, ejecutas una función
         self.Lista_Enlazada.triggered.connect(self.abrir_ventana_lista)
 
     def abrir_ventana_lista(self):
-        # Aquí importas y abres tu otra clase
         from loads.load_lista_enlazada_simple import DialogoListaEnlazada
         self.ventana_secundaria = DialogoListaEnlazada()
         self.ventana_secundaria.show()
